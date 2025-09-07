d111056fcba632cd066c879739e87ee164ebf3b2
Michael Ramirez <45132037+MykeRam@users.noreply.github.com>
2025-09-06 23:50:50 -0400

    trash icon added to cards

--- scripts/index.js
+++ scripts/index.js
@@ -69,9 +69,17 @@ const cardElement = cardTemplate.cloneNode(true);
 const cardImageEl = cardElement.querySelector('.card__image');
 const cardTitleEl = cardElement.querySelector('.card__title');
 const likeButton = cardElement.querySelector('.card__like-button');
+const deleteButton = cardElement.querySelector('.card__delete-button');
+
+
+
 
 likeButton.addEventListener("click", () => {
     likeButton.classList.toggle('card__like-button_active');
+    });
+
+deleteButton.addEventListener("click", () => {
+    cardElement.remove();
     });
 
 

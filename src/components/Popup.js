f1495aeb0a68c4f0e65cb08d4f34831f5c132c48
Michael Ramirez <45132037+MykeRam@users.noreply.github.com>
2025-12-16 22:33:07 -0500

    created files for each class

---
+++ src/components/Popup.js
@@ -0,0 +1,36 @@
+export default class Popup {
+  constructor(popupSelector) {
+    this._popup = document.querySelector(popupSelector);
+    this._handleEscClose = this._handleEscClose.bind(this);
+  }
+
+  open() {
+    this._popup.classList.add("modal_opened");
+    document.addEventListener("keydown", this._handleEscClose);
+  }
+
+  close() {
+    this._popup.classList.remove("modal_opened");
+    document.removeEventListener("keydown", this._handleEscClose);
+  }
+
+  _handleEscClose(evt) {
+    if (evt.key === "Escape") {
+      this.close();
+    }
+  }
+
+  setEventListeners() {
+    this._popup.addEventListener("mousedown", (evt) => {
+      // close on overlay click
+      if (evt.target === evt.currentTarget) {
+        this.close();
+      }
+
+      // close on close button click
+      if (evt.target.classList.contains("modal__close")) {
+        this.close();
+      }
+    });
+  }
+}
